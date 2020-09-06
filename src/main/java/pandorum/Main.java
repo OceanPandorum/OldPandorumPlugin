6793b60f50d862dcfaf0d22ebf2c7f836dac9dfb
Skat <55407440+skykatik@users.noreply.github.com>
2020-09-06 17:31:45 +0300

    Upload

---
+++ src/main/java/pandorum/Main.java
@@ -0,0 +1,18 @@
+package pandorum;
+
+import arc.util.CommandHandler;
+import mindustry.plugin.Plugin;
+
+public class Main extends Plugin{
+
+    public Main(){}
+
+    @Override
+    public void init(){}
+
+    @Override
+    public void registerServerCommands(CommandHandler handler){}
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){}
+}
