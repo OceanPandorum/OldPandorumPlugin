3658fa889426217ee3cb08cf8d6a36a46b5a3e57
Skat <55407440+skykatik@users.noreply.github.com>
2020-10-19 21:22:57 +0300

    Перевёл на 107 версию

--- src/main/java/pandorum/Main.java
+++ src/main/java/pandorum/Main.java
@@ -8,9 +8,9 @@ import arc.util.Log;
 import arc.util.io.Streams;
 import mindustry.Vars;
 import mindustry.gen.Call;
-import mindustry.plugin.Plugin;
+import mindustry.mod.Plugin;
 import org.hjson.*;
-import java.util.stream.Stream;
+
 import java.io.IOException;
 
 import static mindustry.game.EventType.PlayerJoin;
@@ -32,46 +32,46 @@ public class Main extends Plugin{
         Events.on(PlayerJoin.class, event -> {
             new Updater(event.player);
 
-            Call.onLabel(event.player.con, config.get("title", 1).asString(), 1100f, 284f, 529f);
-            Call.onLabel(event.player.con, config.get("title", 2).asString(), 1100f, 396f, 560f);
-            Call.onLabel(event.player.con, config.get("title", 3).asString(), 1100f, 508f, 529f);
-            Call.onLabel(event.player.con, config.get("title", 4).asString(), 1100f, 508f, 418f);
-            Call.onLabel(event.player.con, config.get("title", 5).asString(), 1100f, 508f, 304f);
-            Call.onLabel(event.player.con, config.get("title", 6).asString(), 1100f, 396f, 304f);
-            Call.onLabel(event.player.con, config.get("title", 7).asString(), 1100f, 284f, 304f);
-            Call.onLabel(event.player.con, config.get("title", 8).asString(), 1100f, 284f, 418f);
+            Call.label(event.player.con, config.get("title", 1).asString(), 1100f, 284f, 529f);
+            Call.label(event.player.con, config.get("title", 2).asString(), 1100f, 396f, 560f);
+            Call.label(event.player.con, config.get("title", 3).asString(), 1100f, 508f, 529f);
+            Call.label(event.player.con, config.get("title", 4).asString(), 1100f, 508f, 418f);
+            Call.label(event.player.con, config.get("title", 5).asString(), 1100f, 508f, 304f);
+            Call.label(event.player.con, config.get("title", 6).asString(), 1100f, 396f, 304f);
+            Call.label(event.player.con, config.get("title", 7).asString(), 1100f, 284f, 304f);
+            Call.label(event.player.con, config.get("title", 8).asString(), 1100f, 284f, 418f);
 
             Vars.net.pingHost(config.get("ip", 1).asString(), config.get("port", 1).asInt(), host -> {
-                    Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 284f, 490f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 284f, 490f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 284f, 490f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 284f, 490f));
 
             Vars.net.pingHost(config.get("ip", 2).asString(), config.get("port", 2).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 396f, 521f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 396f, 521f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 396f, 521f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 396f, 521f));
 
             Vars.net.pingHost(config.get("ip", 3).asString(), config.get("port", 3).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 508f, 490f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 508f, 490f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 508f, 490f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 508f, 490f));
 
             Vars.net.pingHost(config.get("ip", 4).asString(), config.get("port", 4).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 508f, 377f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 508f, 377f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 508f, 377f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 508f, 377f));
 
             Vars.net.pingHost(config.get("ip", 5).asString(), config.get("port", 5).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 508f, 265f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 508f, 265f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 508f, 265f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 508f, 265f));
 
             Vars.net.pingHost(config.get("ip", 6).asString(), config.get("port", 6).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 396f, 265f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 396f, 265f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 396f, 265f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 396f, 265f));
 
             Vars.net.pingHost(config.get("ip", 7).asString(), config.get("port", 7).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 284f, 265f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 284f, 265f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 284f, 265f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 284f, 265f));
 
             Vars.net.pingHost(config.get("ip", 8).asString(), config.get("port", 8).asInt(), host -> {
-                Call.onLabel(event.player.con, "\uE837 [accent]Online " + host.players, 1100f, 284f, 377f);
-            }, e -> Call.onLabel(event.player.con, "[red]Offline", 1100f, 284f, 377f));
+                Call.label(event.player.con, "\uE837 [accent]line " + host.players, 1100f, 284f, 377f);
+            }, e -> Call.label(event.player.con, "[red]Offline", 1100f, 284f, 377f));
 
         });
     }
@@ -102,7 +102,8 @@ public class Main extends Plugin{
 
             dir.child("config.hjson").writeString(object.toString(Stringify.HJSON), false);
             try{
-                Streams.copy(Main.class.getClassLoader().getResourceAsStream("HUB.msav"), Core.settings.getDataDirectory().child("maps/HUB.msav").write(false));
+                Streams.copy(Main.class.getClassLoader().getResourceAsStream("HUB.msav"),
+                             Core.settings.getDataDirectory().child("maps/HUB.msav").write(false));
             }catch(IOException e){
                 Log.err(e);
             }
--- src/main/java/pandorum/Updater.java
+++ src/main/java/pandorum/Updater.java
@@ -1,9 +1,7 @@
 package pandorum;
 
-import arc.util.Log;
 import mindustry.Vars;
-import mindustry.entities.type.Player;
-import mindustry.gen.Call;
+import mindustry.gen.*;
 
 import static pandorum.Main.config;
 
@@ -16,11 +14,12 @@ public class Updater{
             while(!th.isInterrupted()){
                 try{
                     Thread.sleep(100);
-                    if(Vars.playerGroup.all().contains(player))
+                    if(Groups.player.contains(p -> p == player)){
                         checkTile(player);
-                    else
+                    }else{
                         th.interrupt();
-                }catch(InterruptedException ignored){Log.info(ignored);}
+                    }
+                }catch(InterruptedException ignored){}
             }
         });
         th.start();
@@ -29,49 +28,49 @@ public class Updater{
     public void checkTile(Player player){
         if(player.x <= 304 && player.x >= 264 && player.y >= 488 && player.y <= 531){
             Vars.net.pingHost(config.get("ip", 1).asString(), config.get("port", 1).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 1).asString(), config.get("port", 1).asInt());
+                Call.connect(player.con, config.get("ip", 1).asString(), config.get("port", 1).asInt());
             }, e -> {});
         }
 
         if(player.x <= 416 && player.x >= 376 && player.y >= 519 && player.y <= 562){
             Vars.net.pingHost(config.get("ip", 2).asString(), config.get("port", 2).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 2).asString(), config.get("port", 2).asInt());
+                Call.connect(player.con, config.get("ip", 2).asString(), config.get("port", 2).asInt());
             }, e -> {});
         }
 
         if(player.x >= 488 && player.x <= 528 && player.y <= 531 && player.y >= 488){
             Vars.net.pingHost(config.get("ip", 3).asString(), config.get("port", 3).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 3).asString(), config.get("port", 3).asInt());
+                Call.connect(player.con, config.get("ip", 3).asString(), config.get("port", 3).asInt());
             }, e -> {});
         }
 
         if(player.x >= 488 && player.x <= 528 && player.y <= 420 && player.y >= 379){
             Vars.net.pingHost(config.get("ip", 4).asString(), config.get("port", 4).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 4).asString(), config.get("port", 4).asInt());
+                Call.connect(player.con, config.get("ip", 4).asString(), config.get("port", 4).asInt());
             }, e -> {});
         }
 
         if(player.x >= 488 && player.x <= 528 && player.y <= 306 && player.y >= 263){
             Vars.net.pingHost(config.get("ip", 5).asString(), config.get("port", 5).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 5).asString(), config.get("port", 5).asInt());
+                Call.connect(player.con, config.get("ip", 5).asString(), config.get("port", 5).asInt());
             }, e -> {});
         }
 
         if(player.x >= 376 && player.x <= 416 && player.y <= 306 && player.y >= 263){
             Vars.net.pingHost(config.get("ip", 6).asString(), config.get("port", 6).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 6).asString(), config.get("port", 6).asInt());
+                Call.connect(player.con, config.get("ip", 6).asString(), config.get("port", 6).asInt());
             }, e -> {});
         }
 
         if(player.x <= 304 && player.x >= 264 && player.y <= 306 && player.y >= 263){
             Vars.net.pingHost(config.get("ip", 7).asString(), config.get("port", 7).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 7).asString(), config.get("port", 7).asInt());
+                Call.connect(player.con, config.get("ip", 7).asString(), config.get("port", 7).asInt());
             }, e -> {});
         }
 
         if(player.x <= 304 && player.x >= 264 && player.y >= 379 && player.y <= 420){
             Vars.net.pingHost(config.get("ip", 8).asString(), config.get("port", 8).asInt(), host -> {
-                Call.onConnect(player.con, config.get("ip", 8).asString(), config.get("port", 8).asInt());
+                Call.connect(player.con, config.get("ip", 8).asString(), config.get("port", 8).asInt());
             }, e -> {});
         }
     }
