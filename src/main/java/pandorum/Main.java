f0a570e8bfa488395f335aea231a377cb55c9c07
Skat <55407440+skykatik@users.noreply.github.com>
2020-12-10 19:28:06 +0300

    Новая фича: оповещение о постройке реактора вблизи к ядру

--- src/main/java/pandorum/Main.java
+++ src/main/java/pandorum/Main.java
@@ -1,42 +1,51 @@
 package pandorum;
 
-import arc.Core;
-import arc.Events;
+import arc.*;
 import arc.files.Fi;
-import arc.struct.Seq;
+import arc.struct.*;
 import arc.util.*;
-import components.Bundle;
-import components.Config;
+import components.*;
 import mindustry.content.Blocks;
 import mindustry.core.NetClient;
-import mindustry.game.EventType;
-import mindustry.game.Team;
-import mindustry.game.Teams;
-import mindustry.gen.Call;
-import mindustry.gen.Groups;
-import mindustry.gen.Player;
-import mindustry.gen.Unit;
+import mindustry.game.EventType.*;
+import mindustry.game.*;
+import mindustry.gen.*;
 import mindustry.mod.Plugin;
-import mindustry.type.Item;
-import mindustry.type.UnitType;
+import mindustry.type.*;
 import mindustry.world.Block;
 import mindustry.world.blocks.storage.CoreBlock;
 
 import static mindustry.Vars.*;
 
 public class Main extends Plugin{
+    private static final double ratio = 0.6;
+
     public static final Fi dir = Core.settings.getDataDirectory().child("/mods/pandorum/");
     public static final Config config = new Config();
     public static final Bundle bundle = new Bundle();
-    private static final double ratio = 0.6;
+
     private final Seq<String> votes = new Seq<>();
+    private final ObjectSet<String> alertIgnores = new ObjectSet<>();
+
+    private Interval alertInterval = new Interval();
 
     public Main(){}
 
     @Override
     public void init(){
+        Events.on(BuildSelectEvent.class, event -> {
+            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null &&
+               event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer() &&
+               event.team.cores().contains(c -> event.tile.dst(c.x, c.y) < 350)){
+                Player target = event.builder.getPlayer();
 
-        Events.on(EventType.PlayerLeave.class, event -> {
+                if(alertInterval.get(200)){
+                    Groups.player.each(p -> !alertIgnores.contains(p.uuid()), player -> player.sendMessage(bundle.format("events.alert", target.name, event.tile.x, event.tile.y)));
+                }
+            }
+        });
+
+        Events.on(PlayerLeave.class, event -> {
             int cur = votes.size;
             int req = (int) Math.ceil(ratio * Groups.player.size());
             if(votes.contains(event.player.uuid())){
@@ -46,7 +55,7 @@ public class Main extends Plugin{
             }
         });
 
-        Events.on(EventType.GameOverEvent.class, event -> votes.clear());
+        Events.on(GameOverEvent.class, event -> votes.clear());
     }
 
     @Override
@@ -60,6 +69,16 @@ public class Main extends Plugin{
 
     @Override
     public void registerClientCommands(CommandHandler handler){
+        handler.<Player>register(bundle.get("alert.name"), bundle.get("alert.description"), (args, player) -> {
+            if(alertIgnores.contains(player.uuid())){
+                alertIgnores.remove(player.uuid());
+                Info.text(player, "$alert.on");
+            }else{
+                alertIgnores.add(player.uuid());
+                Info.text(player, "$alert.off");
+            }
+        });
+
         // слегка переделанный rtv
         handler.<Player>register(bundle.get("rtv.name"), bundle.get("rtv.description"), (args, player) -> {
             if(player.uuid() != null && votes.contains(player.uuid())){
@@ -78,7 +97,7 @@ public class Main extends Plugin{
 
             votes.clear();
             Call.sendMessage(bundle.get("rtv.successful"));
-            Events.fire(new EventType.GameOverEvent(Team.crux));
+            Events.fire(new GameOverEvent(Team.crux));
         });
 
         //Отправка сообщения для всех в отдельном окне
@@ -96,7 +115,7 @@ public class Main extends Plugin{
                 Info.text(player, "$commands.permission-denied");
                 return;
             }
-            Events.fire(new EventType.GameOverEvent(Team.crux));
+            Events.fire(new GameOverEvent(Team.crux));
         });
 
         //Заспавнить юнитов
